65bc1c90a6c88d7e96d42f4b86283614d2a4899c
Jxestar <71887932+Jxestar@users.noreply.github.com>
2021-04-04 21:05:54 +0700

    fixed ping command 🤦‍♂️

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ client.on('message', message => {
 
 //ping command
   if (command === 'ping') {
-    const ping = message.createdTimestamp;
+    const ping = Date.now() - message.createdTimestamp
     message.channel.send(`🏓 Pong! Latency is **" + ping + "** ms!`)
   }
 	
